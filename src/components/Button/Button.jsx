ac3969d9c57dce448d827e5d8aa92b98befbf6d8
Nourhassann <nourhassan132@gmail.com>
2022-12-28 07:18:37 +0200

    first commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,4 @@
+import "./styles.css";
+export const Button = () => {
+  return <button className="btn">example</button>;
+};
---
+++ src/stories/Requirements.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Button } from "../components/Button";
+
+const stories = storiesOf("Components", module);
+stories.add("Button", () => {
+  return <Button />;
+});
